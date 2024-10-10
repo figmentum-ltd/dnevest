619cf7d804853112d54ddf83cdbc43faed715a76
Bozhidar Manev <95079544+bmanev18@users.noreply.github.com>
2024-10-10 22:18:06 +0200

    Initial component commit

---
+++ dnevest/src/lib.rs
@@ -0,0 +1,16 @@
+#[allow(warnings)]
+#[rustfmt::skip]
+mod bindings;
+
+use bindings::Guest;
+
+struct Component;
+
+impl Guest for Component {
+    /// Say hello!
+    fn hello_world() -> String {
+        "Hello, World!".to_string()
+    }
+}
+
+bindings::export!(Component with_types_in bindings);
