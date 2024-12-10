b28ca3eea233d0ffe92365b2a254c2df4c653434
Katerina Maneva <k.g.maneva@gmail.com>
2024-12-10 09:41:27 +0200

    Implement UncheckedWishCard

--- src/order/wish_card.rs
+++ src/order/wish_card.rs
@@ -1,6 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-use super::{cover::Cover, Error, Result};
+use std::{marker::PhantomData, result::Result as StdResult};
+
+use crate::Storage;
+
+use super::{
+    cover::{Cover, UncheckedCover},
+    Error, Result,
+};
 
 #[cfg_attr(test, derive(Debug, PartialEq))]
 #[derive(Serialize, Deserialize)]
@@ -35,7 +42,7 @@ impl WishCard {
         }
     }
 
-    pub(super) fn check(&self, max_cards: MaxCards) -> Result<()> {
+    pub(super) fn invariant_held(&self, max_cards: MaxCards) -> Result<()> {
         if self.template_id > max_cards.0 {
             Err(Error::InvalidCard)
         } else {
@@ -77,11 +84,69 @@ pub(super) enum Frame {
     Wooden,
 }
 
+#[derive(Deserialize)]
+#[serde(bound = "")]
+pub(super) struct UncheckedWishCard<S>
+where
+    S: Storage + Default,
+{
+    covers: UncheckedCover<S>,
+    background: Rgb,
+    frame: Frame,
+    message: String,
+    font_type: String,
+    font_size: u8,
+    template_id: u8,
+    #[serde(skip)]
+    _storage: PhantomData<S>,
+}
+
+impl<S> UncheckedWishCard<S>
+where
+    S: Storage + Default,
+{
+    fn into_checked(self) -> Result<WishCard> {
+        self.covers.try_into().map(|covers| {
+            WishCard::new_unchecked(
+                covers,
+                self.background,
+                self.frame,
+                self.message,
+                self.font_type,
+                self.font_size,
+                self.template_id,
+            )
+        })
+    }
+}
+
+impl<S> TryFrom<UncheckedWishCard<S>> for WishCard
+where
+    S: Storage + Default,
+{
+    type Error = Error;
+
+    fn try_from(unchecked: UncheckedWishCard<S>) -> StdResult<Self, Self::Error> {
+        S::default()
+            .retrieve("max_cards")
+            .ok_or(Error::NotFound("Failed to fetch the max cards.".into()))
+            .and_then(|max_cards| {
+                serde_json::from_slice(&max_cards)
+                    .map_err(Error::DeserializationFault)
+                    .and_then(|max_cards| {
+                        unchecked
+                            .into_checked()
+                            .and_then(|obj| obj.invariant_held(max_cards).map(|()| obj))
+                    })
+            })
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use crate::{newspaper::Signature, order::cover::Cover};
+    use crate::{newspaper::Signature, order::cover::Cover, services::MockHost};
 
-    use super::{Frame, Result, Rgb, WishCard};
+    use super::{Frame, Result, Rgb, UncheckedWishCard, WishCard};
 
     #[test]
     fn unchecked_deserialization() {
@@ -98,6 +163,19 @@ mod test {
         );
 
         assert_eq!(expected, unchecked)
+    }
+
+    #[test]
+    fn checked_deserialization() {
+        let _host = MockHost::default();
+
+        let json = r#"{"covers":{"preference":"В1616","options":["В4667",null]},"background":[255,0,0],"frame":"White","message":"Честит рожден ден!","font_type":"Times New Roman","font_size":12,"template_id":41}"#;
+        let unchecked: UncheckedWishCard<MockHost> =
+            serde_json::from_str(json).expect("failed to deserialize JSON");
+
+        let res: Result<WishCard> = unchecked.try_into();
+
+        assert_err(res, "The card number does not exist");
     }
 
     #[test]
@@ -118,9 +196,9 @@ mod test {
         )
     }
 
-    // fn assert_err(r: Result<WishCard>, msg: &str) {
-    //     assert!(r.expect_err("expected an error").to_string().contains(msg))
-    // }
+    fn assert_err(r: Result<WishCard>, msg: &str) {
+        assert!(r.expect_err("expected an error").to_string().contains(msg))
+    }
 
     fn covers() -> Cover {
         Cover::new_unchecked(
